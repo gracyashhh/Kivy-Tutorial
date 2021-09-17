69e37c1b1630569ef37b769194ae45eca8ea714e
gracyashhh <gracyashhh@gmail.com>
2021-09-17 21:51:07 +0530

    Toggle buttons, controlling it, disable property

--- main.py
+++ main.py
@@ -6,17 +6,30 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 # from kivy.uix.scrollview import ScrollView
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty,BooleanProperty
 from kivy.metrics import dp
 from kivy.uix.button import Button
 
 class WidgetsExample(GridLayout):
     my_text=StringProperty("1")
     count=1
+    flag=BooleanProperty(False)
     def on_button_click(self):
         print("Button clicked")
-        self.count+=1
-        self.my_text=str(self.count)
+        if self.flag:
+            self.count+=1
+            self.my_text=str(self.count)
+
+    def on_toggle_button_state(self,toggle_button):
+
+        print("toggle state "+toggle_button.state)
+        if toggle_button.state=="down":
+            toggle_button.text="ON"
+            self.flag=True
+        else:
+            toggle_button.text="OFF"
+            self.flag=False
+
 
 # class ScrollViewExample(ScrollView):
 #     pass
